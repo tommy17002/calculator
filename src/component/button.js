a7783f0504bb63e95d0b24246b7771d754314f7a
enigma <tommy17002@mail.unpad.ac.id>
2024-08-27 03:21:21 +0700

    button done

--- src/component/button.js
+++ src/component/button.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const button = ({val, grow=false, operator=false}) => {
+const button = ({val, clickAction, grow=false, operator=false}) => {
   return (
     <button
+        onClick={() => clickAction(val)}
      className={`text-white text-2xl h-1/5 cursor-pointer border border-gray-900 hover:bg-gray-700
         ${grow ? 'flex-[2_0_42%]' : 'flex-[1_0_21%]'}
         ${grow && val === 0 ? 'flex-[3_0_63%]' : 'flex-[1_0_21%]'}
--- src/component/calc.js
+++ src/component/calc.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './button';
 
 const calc = () => {
+    const [firstNumber, setFirstNumber] = useState(null);
+    const [secondNumber, setSecondNumber] = useState(null);
+    const [operator, setOperator] = useState(null);
+    const [displayNumber, setDisplayNumber] = useState(null);
+    const [result, setResult] = useState (null)
+    const handleNumber = (val) => {
+        console.log(val);
+    };
+    const handleOperator = (val) => {
+        console.log(`operator ${val}`);
+    };
+    const handleClear = () => {
+        console.log('clear');   
+    };
+    const handleResult = () => {
+        console.log('result');
+    };
   return (
     <div className='w-[320px] h-[245px] bg-gray-800'>
         <div className='flex h-[100px] items-center justify-end text-white'>
@@ -9,30 +26,30 @@ const calc = () => {
         </div>
         <div className='flex flex-row flex-wrap h-[345]'>
             {/* first row */}
-            <Button val={1}/>
-            <Button val={2}/>
-            <Button val={3}/>
-            <Button val={'+'} operator={true} />
+            <Button clickAction={handleNumber} val={1}/>
+            <Button clickAction={handleNumber} val={2}/>
+            <Button clickAction={handleNumber} val={3}/>
+            <Button clickAction={handleOperator} val={'+'} operator={true} />
             
             {/* second row */}
-            <Button val={4}/>
-            <Button val={5}/>
-            <Button val={6}/>
-            <Button val={'-'} operator={true} />
+            <Button clickAction={handleNumber} val={4}/>
+            <Button clickAction={handleNumber} val={5}/>
+            <Button clickAction={handleNumber} val={6}/>
+            <Button clickAction={handleOperator} val={'-'} operator={true} />
 
             {/* third row */}
-            <Button val={7}/>
-            <Button val={8}/>
-            <Button val={9}/>
-            <Button val={'*'} operator={true} />
+            <Button clickAction={handleNumber} val={7}/>
+            <Button clickAction={handleNumber} val={8}/>
+            <Button clickAction={handleNumber} val={9}/>
+            <Button clickAction={handleOperator} val={'*'} operator={true} />
 
             {/* forth row */}
-            <Button val={0} grow={true} />
-            <Button val={'/'} operator={true} />
+            <Button clickAction={handleNumber} val={0} grow={true} />
+            <Button clickAction={handleOperator} val={'/'} operator={true} />
 
             {/* fifth row */}
-            <Button val={'C'} grow={true} />
-            <Button val={'='} grow={true} operator={true} />
+            <Button clickAction={handleClear} val={'C'} grow={true} />
+            <Button clickAction={handleResult} val={'='} grow={true} operator={true} />
         </div>
     </div>
     
